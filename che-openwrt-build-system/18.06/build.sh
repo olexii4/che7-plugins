6296ac69aec81dac03778c07fea044c48c0726fb
Oleksii Orel <oorel@redhat.com>
2019-03-01 15:01:17 +0200

    fix openwrt-build-system
    Signed-off-by: Oleksii Orel <oorel@redhat.com>

--- che-openwrt-build-system/18.06/build.sh
+++ che-openwrt-build-system/18.06/build.sh
@@ -2,10 +2,4 @@
 set -e
 set -u
 
-if [ -z etc/openwrt ]; then
-    cd etc
-    git clone git://git.openwrt.org/openwrt/openwrt.git -b openwrt-18.06
-    cd ../
-fi
-
-cd etc/openwrt && make menuconfig && cd ../../ && docker build -f Dockerfile -t olexii4dockerid/openwrt-x86-generic-rootfs:18.06 .
+docker build -f Dockerfile -t olexii4dockerid/openwrt-x86-generic-rootfs:18.06 .
